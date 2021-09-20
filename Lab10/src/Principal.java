293a2a4c94f26a98c53edd8d3e65d464fab0ce3d
GMichaloski <gustavomichaloski@gmail.com>
2021-09-20 10:06:44 -0300

    Lab10 20/09/2021

--- Lab10/src/Principal.java
+++ Lab10/src/Principal.java
@@ -3,21 +3,21 @@ import java.util.ArrayList;
 public class Principal {
 
     public static void main(String[] args) {
-        Sorteador sorteadorJ1 = new DadosDeGamao();
-        Sorteador sorteadorJ2 = new DadosTriplos();
+        DadosDeGamao sorteadorJ1 = new DadosDeGamao();
+        DadosTriplos sorteadorJ2 = new DadosTriplos();
 
 
-        JogoMalucoComSorteadores JM = new JogoMalucoComSorteadores("JogoMuitoMaluco",
+        JogoMalucoComSorteadores<DadosDeGamao,DadosTriplos> jm = new JogoMalucoComSorteadores<>("JogoMuitoMaluco",
                 "Guga", "Vini", 1,
                 sorteadorJ1, sorteadorJ2);
         for (int numeroDeRodadas = 1; numeroDeRodadas <= 100000; numeroDeRodadas *= 2) {
-            JM.setNumeroDeRodadas(numeroDeRodadas);
-            JM.jogar();
+            jm.setNumeroDeRodadas(numeroDeRodadas);
+            jm.jogar();
 
-            System.out.println(JM.obterResultadoUltimoJogo());
+            System.out.println(jm.obterResultadoUltimoJogo());
         }
-        System.out.println("Porcentagem de vitórias do Jogador 1: " + JM.getPercentualVitoriasJogador1());
-        System.out.println("Porcentagem de vitórias do Jogador 2: " + JM.getPercentualVitoriasJogador2());
-        System.out.println("Porcentagem de empates: " + JM.getPercentualEmpates());
+        System.out.println("Porcentagem de vitórias do Jogador 1: " + jm.getPercentualVitoriasJogador1());
+        System.out.println("Porcentagem de vitórias do Jogador 2: " + jm.getPercentualVitoriasJogador2());
+        System.out.println("Porcentagem de empates: " + jm.getPercentualEmpates());
     }
 }
